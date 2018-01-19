1c428cc1d267e1ac4e20059fe3433b17f05b8750
Cristi Constantin <cristi.constantin@live.com>
2018-01-20 00:45:26 +0200

    feat(ducklist): can list files and folders

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const execa = require('execa')
+
+async function ducklist (uri, options = {}) {
+  /**
+   * Run duck.sh longlist command.
+   * Can list FTP, FTPS and SFTP.
+   */
+  const config = { retry: 3 }
+  options = { ...config, ...options }
+
+  const cmd = `duck -y --retry ${options.retry} --longlist "${uri}"`
+
+  try {
+    const result = await execa.shell(cmd)
+    return parseList(result.stdout)
+  } catch (err) {
+    throw err
+  }
+}
+
+function parseList (text) {
+  /**
+   * Parse duck.sh listing files.
+   */
+  const regex = /^(?:[drwx-]+?\t+[\w\W]+?\t+)([\w\W]+?)$/gm
+  const output = []
+  let matches
+
+  while ((matches = regex.exec(text))) {
+    output.push(matches[1].trim())
+  }
+  return output
+}
+
+module.exports = {
+  ducklist,
+  parseList
+}
