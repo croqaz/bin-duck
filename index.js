af95c80b99a148dd9ffd3308d75cf68d170fc433
Cristi Constantin <cristi.constantin@live.com>
2018-01-22 00:17:24 +0200

    feat: implemented upload and download todo: testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const execa = require('execa')
 
-async function ducklist (uri, options = {}) {
+async function list (uri, options = {}) {
   /**
    * Run duck.sh longlist command.
    * Can list FTP, FTPS and SFTP.
@@ -8,10 +8,8 @@ async function ducklist (uri, options = {}) {
   const config = { retry: 3 }
   options = { ...config, ...options }
 
-  const cmd = `duck -y --retry ${options.retry} --longlist "${uri}"`
-
   try {
-    const result = await execa.shell(cmd)
+    const result = await execa('duck', ['--nokeychain', '-yq', '-r', options.retry, '--longlist', uri])
     return parseList(result.stdout)
   } catch (err) {
     throw err
@@ -32,7 +30,49 @@ function parseList (text) {
   return output
 }
 
+async function download (uri, dest = '.', options = {}) {
+  /**
+   * Run duck.sh download command.
+   * Denote a folder with a trailing '/'.
+   */
+  const config = { retry: 3, parallel: 3 }
+  options = { ...config, ...options }
+
+  // Suppress progress messages
+  // Do not save passwords in keychain
+  // Retry failed connection attempts
+  // Use concurrent connections for transfers
+  const cmd = `duck --nokeychain -yq -r ${options.retry} --parallel ${options.parallel} --download "${uri}" "${dest}"`
+
+  try {
+    const result = await execa.shell(cmd)
+    return result
+  } catch (err) {
+    throw err
+  }
+}
+
+async function upload (uri, path, options = {}) {
+  /**
+   * Run duck.sh upload command.
+   * Uploads file or folder recursively.
+   */
+  const config = { retry: 3, parallel: 3 }
+  options = { ...config, ...options }
+
+  const cmd = `duck --nokeychain -yq -r ${options.retry} --parallel ${options.parallel} --upload "${uri}" "${path}"`
+
+  try {
+    const result = await execa.shell(cmd)
+    return result
+  } catch (err) {
+    throw err
+  }
+}
+
 module.exports = {
-  ducklist,
-  parseList
+  list,
+  parseList,
+  download,
+  upload
 }
